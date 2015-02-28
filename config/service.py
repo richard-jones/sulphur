1536642e6cfa81d0733dcdaec61dd276663593fc
Richard Jones <richard@cottagelabs.com>
2015-02-28 10:27:04 +0000

    production by country by year

--- config/service.py
+++ config/service.py
@@ -1,6 +1,6 @@
 # overrides for the webapp deployment
 DEBUG = True
-PORT = 5000
+PORT = 5021
 SSL = False
 THREADED = True
 
@@ -14,5 +14,36 @@ ELASTIC_SEARCH_VERSION = "1.4.2"
 # Classes from which to retrieve ES mappings to be used in this application
 ELASTIC_SEARCH_MAPPINGS = [
     "service.dao.ProductionDAO",
-    "service.dao.ConsumptionDAO"
+    "service.dao.ConsumptionDAO",
+    "service.dao.TradeDAO"
 ]
+
+# Query route configuration
+QUERY_ROUTE = {
+    "query" : {                            # the URL route at which it is mounted
+        "prod" : {                     # the URL name for the index type being queried
+            "auth" : False,                     # whether the route requires authentication
+            "role" : None,                      # if authenticated, what role is required to access the query endpoint
+            "filters" : [],                     # names of the standard filters to apply to the query
+            "dao" : "service.dao.ProductionDAO"       # classpath for DAO which accesses the underlying ES index
+        }
+    }
+}
+
+CLIENTJS_PROD_QUERY_ENDPOINT = "/query/prod"
+
+AUTOCOMPLETE_TERM = {
+    "country" : {
+        "filter" : {
+            "country" : {
+                "start_wildcard" : True,
+                "end_wildcard" : True
+            }
+        },
+        "facet" : "country.exact",
+        "input_filter" : lambda x : x,
+        "default_size" : 10,
+        "max_size" : 25,
+        "dao" : "service.dao.ProductionDAO"
+    }
+}
--- service/web.py
+++ service/web.py
@@ -47,14 +47,6 @@ app.register_blueprint(crud, url_prefix="/api")
 from octopus.modules.es.query import blueprint as query
 app.register_blueprint(query, url_prefix="/query")
 
-# Sherpa Fact integration endpoint
-from octopus.modules.sherpafact.proxy import blueprint as fact
-app.register_blueprint(fact, url_prefix="/fact")
-
-# Example usages of modules
-from octopus.modules.examples.examples import blueprint as examples
-app.register_blueprint(examples, url_prefix="/examples")
-
 from octopus.modules.clientjs.fragments import blueprint as fragments
 app.register_blueprint(fragments, url_prefix="/frag")
 
